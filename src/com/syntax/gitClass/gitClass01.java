b59358391e47be79cab455992b3c02a1a4d6ae14
HoshiiKhrystyna <kristinagoshiy@gmail.com>
2022-06-01 20:30:43 -0500

    modified the git class

--- src/com/syntax/gitClass/gitClass01.java
+++ src/com/syntax/gitClass/gitClass01.java
@@ -3,7 +3,7 @@ package com.syntax.gitClass;
 public class gitClass01 {
 	public static void main(String[] args) {
 		System.out.println("popculture");
-		
+		System.out.println("abrakedabra");
 		
 		
 	}
